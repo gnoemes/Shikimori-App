dd7f3e74dfcf4cc7208497a8574807788f3af056
WINDOWS-MT88Q05\User <hydro.ea@list.ru>
2018-08-03 18:55:46 +0400

    added feature for fast back on double tap at bottom menu

--- app/src/main/java/com/gnoemes/shikimoriapp/presentation/view/main/MainView.java
+++ app/src/main/java/com/gnoemes/shikimoriapp/presentation/view/main/MainView.java
@@ -2,6 +2,7 @@ package com.gnoemes.shikimoriapp.presentation.view.main;
 
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.AddToEndStrategy;
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.gnoemes.shikimoriapp.presentation.view.common.activity.BaseView;
 
@@ -16,4 +17,19 @@ public interface MainView extends BaseView {
      */
     @StateStrategyType(AddToEndSingleStrategy.class)
     void initBottomNavigation();
+
+    @StateStrategyType(SkipStrategy.class)
+    void clearMenuBackStack();
+
+    @StateStrategyType(SkipStrategy.class)
+    void clearSocialBackStack();
+
+    @StateStrategyType(SkipStrategy.class)
+    void clearSearchBackStack();
+
+    @StateStrategyType(SkipStrategy.class)
+    void clearCalendarBackStack();
+
+    @StateStrategyType(SkipStrategy.class)
+    void clearFavoriteBackStack();
 }
